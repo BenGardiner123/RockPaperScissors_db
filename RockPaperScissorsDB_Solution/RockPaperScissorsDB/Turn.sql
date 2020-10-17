c54b3e72f30317b0dcaf6a8241c7bf05342c3b0b
Ben Gardiner <bennygards@gmail.com>
2020-10-17 23:03:42 +1100

    adding some tables

---
+++ RockPaperScissorsDB_Solution/RockPaperScissorsDB/Turn.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Turn]
+(
+	[TurnId] DATETIME NOT NULL, 
+	[username] NVARCHAR(50) NOT NULl, 
+    [PlayersSelection] NVARCHAR(10) NOT NULL, 
+    [AiSelection] NVARCHAR(10) NOT NULL,
+	PRIMARY KEY (username, TurnId), 
+	FOREIGN KEY (username) references Player
+)
